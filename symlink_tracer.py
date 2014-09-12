a05f9ec4efdd00f02d012462f5ddf896102e08bf
George Chen <george.guo.chen@outlook.com>
2014-09-12 16:35:07 +0800

    Bug fixing & enhancement
    Enhancement for handling the situation when a user specifies a path
    whose parent directories are actually symbolic links. This will cause
    the iterator (PathIter) never goes to the link's real path, causing
    hanging.
    
    After fix, if any of the link has its parents as link, the tracer will
    restarting from that link from parent directories.

--- symlink_tracer.py
+++ symlink_tracer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os, sys
 
+# For colourful fancy outputs
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -19,6 +20,11 @@ class PathIter(object):
         out=os.path.abspath(self.check_path)
         if self.check_path.endswith(os.sep):
             self.check_path=self.check_path.rstrip(os.sep) # Right strip the os.sep if the check_path ends with it. In case of the link is created as 'ln -s xx/xx/ xx'
+        eval_path, valid = validatePath(self.check_path)
+        # print eval_path, valid # for debugging
+        if valid == False:
+            print("%sOne of the parent directories is symbolic link, retracing from it.%s"%(bcolors.WARNING,bcolors.ENDC))
+            self.check_path=eval_path
         tmp_path=os.path.dirname(self.check_path)
         if not tmp_path:
             tmp_path=os.getcwd()
@@ -27,23 +33,49 @@ class PathIter(object):
         if os.path.islink(to_check):
             self.check_path=os.readlink(to_check)
         return out
-      
+       
 def getLinks(link_name):
     cwd_save=os.getcwd()
     target=os.path.realpath(link_name)
     iter=PathIter(link_name)
     list=[]
     test= iter.next()
+    last=""
     arrow=">>"
     count=0
-    while(test!=target):
+    while(test!=target and last!=test):
         list.append(arrow*count+" "+test)
+        last=test
         test=iter.next()
         count+=1
     list.append(arrow*count+" "+bcolors.CYAN+test+bcolors.ENDC)
-    for i in list:
-       print i
-    os.chdir(cwd_save)
+    for path in list:
+       print path
+    os.chdir(cwd_save)  # returning original path
+
+def validatePath(input_path):
+    """
+    validatePath() is to check if any parent directories of
+    given path is a symbolic link. If so, return the eldest 
+    path and False, otherwise return input path and True.
+    """
+    parent=os.path.dirname(os.path.abspath(input_path))
+    path_ancestry_list=[]
+    while(parent!=os.sep):
+        if os.path.islink(parent):
+            path_ancestry_list.append((parent,True))
+        else:
+            path_ancestry_list.append((parent,False))
+        parent=os.path.dirname(parent)
+    #    print path_ancestry_list[-1]       # for debugging
+    eldest=path_ancestry_list[0][0]
+    if True not in [i[1] for i in path_ancestry_list]:
+        return os.path.abspath(input_path), True
+    else:
+        for i in range(len(path_ancestry_list)):
+            if path_ancestry_list[i][1] == True:
+                eldest=path_ancestry_list[i][0]
+        return eldest, False
 
 if __name__ == "__main__":
     argv_list = sys.argv[1:]
